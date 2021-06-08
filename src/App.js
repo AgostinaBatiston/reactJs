a7ae21f99c77d661a8a7fbdf3ea8ca3abceaf592
Agostina Batiston <agbatiston@hotmail.com>
2021-06-08 20:08:20 -0300

    Agregue el navbar con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-
+import React, { Component } from 'react'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Libreria uzumaki</h1>
 
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      
+
+      <div className='App'>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Libreria Uzumaki</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            
+          </Navbar.Collapse>
+        </Navbar>
+       
+      </div>
+    )
+  }
 }
 
 export default App;
