6521a71f6e83c461ea36b190afe7b1b7951b51b7
Agostina Batiston <agbatiston@hotmail.com>
2021-06-08 22:59:41 -0300

    Agregue semantic ui al proyecto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar } from 'react-bootstrap';
-import { Nav } from 'react-bootstrap';
+
+
 
 
 class App extends Component {
@@ -11,17 +10,6 @@ class App extends Component {
       
 
       <div className='App'>
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="#home">Libreria Uzumaki</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-            </Nav>
-            
-          </Navbar.Collapse>
-        </Navbar>
        
       </div>
     )
