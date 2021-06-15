14b6c56916d590a0c9d9ae08de25efeaeed632e9
Agostina Batiston <agbatiston@hotmail.com>
2021-06-15 12:27:04 -0300

    Hice un ItemCount.js para agregarlo en los products

---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './ItemCount.css';
+
+export default class ItemCount extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            count: 0,
+        };
+    }
+
+
+    render() {
+        return (
+
+            <div>
+                
+            </div>
+        )
+    }
+}
