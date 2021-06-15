2b14b8bbc0fa5972e2d4abc55bbd0ba56601fcb7
Agostina Batiston <agbatiston@hotmail.com>
2021-06-15 13:12:01 -0300

    Le puse los botones para sumar y restar los productos

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
 import './ItemCount.css';
 
 export default class ItemCount extends Component {
@@ -8,15 +9,31 @@ export default class ItemCount extends Component {
         this.state = {
             count: 0,
         };
+
+       
+        
     }
 
+    handleCounterUp = () => {
+        
+        this.setState({ count: this.state.count + 1 });
+      };
+    
+      handleCounterDown = () => {
+        this.setState({ count: this.state.count - 1 });
+      };
 
     render() {
         return (
 
-            <div className="btn-count">
+           <>
+           <div className="btn-count">
                 {this.state.count}
             </div>
+          
+            <Button positive onClick={this.handleCounterUp}> + </Button>
+                <Button negative onClick={this.handleCounterDown}> - </Button>
+            </>
         )
     }
 }
